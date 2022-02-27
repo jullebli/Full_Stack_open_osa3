9ef39678850ecb6d188321b8e254c30812c14f00
Julia Bergman <julia.bergman@helsinki.fi>
2022-02-27 15:57:59 +0200

    Tee 3.3

--- index.js
+++ index.js
@@ -29,6 +29,17 @@ let persons = [
   }
 ]
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
