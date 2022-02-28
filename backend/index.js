11976f208796e4d34bf0b89e20dbd608bcbae634
Julia Bergman <julia.bergman@helsinki.fi>
2022-02-28 20:13:34 +0200

    Tee 3.15

--- backend/index.js
+++ backend/index.js
@@ -14,31 +14,38 @@ morgan.token('post_data', function(req) {
 })
 
 
-
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :post_data"))
 app.use(cors())
-app.use(express.static('build'))
+
 
 
 app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then(person => {
+  Person.findById(req.params.id)
+  .then(person => {
     if (person) {
-    res.json(person)
+      res.json(person)
     } else {
-    res.status(404).end()
+      res.status(404).end()
     }
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(400).send({
+      error: "malformatted id"
+    })
   })
 })
 
-/*
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(p => p.id !== id)
 
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(202).end()
+    })
 })
-*/
+
 
 app.post('/api/persons', (req, res) => {
   app.use(morgan('post_data'))
